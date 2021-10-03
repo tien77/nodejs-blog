1ba61daa0cb213c660719508651a412949aec0b9
Minh Tien <tranminhtien22@gmail.com>
2021-10-03 10:45:33 +0700

    add file

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/blog', (req, res) => {
+    var a = 1,
+        b = 2;
+    var c = a + b;
+
+    res.send('Page Blog edit')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
